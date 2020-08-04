e670e6c82e2a62ad0ace7b19dae99dd0e67df27e
Joshua Ecklund <m.prowler@gmail.com>
2020-08-04 10:04:43 -0600

    Implement log reader

--- app.js
+++ app.js
@@ -2,19 +2,21 @@ const PORT = 8080;
 
 let express = require('express');
 let logger = require('morgan');
-
 let logRouter = require('./routes/log');
 
 let app = express();
 
+// Setup dependencies
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+// Setup routes
 app.use('/log', logRouter);
 
+// Listen for connections
 app.listen(PORT, function() {
-    console.log('Node server started');
+    console.debug('Node server started');
 });
 
 module.exports = app;
--- routes/log.js
+++ routes/log.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const lineReader = require('reverse-line-reader');
 const router = express.Router();
+const fs = require('fs');
 
 const LOG_BASE = '/var/log';
 
@@ -11,24 +11,74 @@ router.get('/:log', function(req, res, next) {
   let filter = req.query.filter || '';
   let lineCount = 0;
 
+  // Check arguments
   if (!log) {
     throw new Error('Missing required param "log"');
   }
 
   let filename = LOG_BASE + '/' + log;
-  lineReader.eachLine(filename, function(line, last) {
+  let body = '';
 
-    if (filter && line.indexOf(filter) === -1) {
-      return;
+  // Open log file
+  fs.open(filename, 'r', function(err, fd) {
+    let chr = '';
+    let line = '';
+    let done = false;
+
+    if (err) {
+      console.error('Error opening specified log file.');
+      throw err;
     }
 
-    res.send(line);
+    // Get file size and allocate buffer instance
+    let stats = fs.statSync(filename);
+    let pos = stats["size"];
+    let buffer = Buffer.alloc(1);
 
-    if (lines === true && ++lineCount >= lines) {
-      return false;
-    }
+    // Iterate through log file in reverse
+    do {
+      let byteCount = fs.readSync(fd, buffer, {
+        length: 1,
+        position: --pos
+      });
+
+      if (byteCount < 1) {
+        // Nothing read from file, we're done
+        done = true;
+      } else {
+        chr = buffer.toString()[0];
+      }
+
+      // Check for line feed
+      if (chr !== "\n") {
+        line = chr + line;
+      } else {
+        // Check for filter string (if specified)
+        if (!filter || line.indexOf(filter) !== -1) {
+          body = body + line + "\n";
+          lineCount++;
+        }
+
+        // Reset line value
+        line = '';
+
+        // Check if we've reached our desired line count
+        if (lines !== false && lineCount >= lines) {
+          done = true;
+        }
+      }
+    } while (!done && pos > 0);
+
+    // Close file handle
+    fs.close(fd, function (err) {
+      if (err) {
+        console.error('Error closing log file');
+        throw err;
+      } else {
+        res.send(body);
+      }
+    });
   });
-
 });
 
 module.exports = router;
